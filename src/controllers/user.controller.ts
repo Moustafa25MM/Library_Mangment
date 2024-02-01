35089d7b6bfae0333b3417b079cf0bf0339d589d
Moustafa25MM <107191669+Moustafa25MM@users.noreply.github.com>
2024-02-01 03:30:05 +0200

    user login

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,6 +1,7 @@
 import prisma from '../database';
 import { Request, Response } from 'express';
 import bcrypt from 'bcrypt';
+import { authMethods } from '../middlewares/auth';
 
 export const registerUser = async (request: Request, response: Response) => {
   try {
@@ -25,6 +26,38 @@ export const registerUser = async (request: Request, response: Response) => {
     return response.status(201).json({ msg: 'user created successfully' });
   } catch (error) {
     console.log('error occured in create user', error);
+    throw error;
+  }
+};
+
+export const loginUser = async (request: Request, response: Response) => {
+  try {
+    const { email, password } = request.body;
+    const existingUser = await prisma.borrower.findUnique({
+      where: { email },
+    });
+    if (!existingUser) {
+      return response.status(401).json({ error: 'user does not exist' });
+    }
+
+    const comparePassword = await bcrypt.compare(
+      password,
+      existingUser.password
+    );
+
+    if (!comparePassword) {
+      response.status(401).json({ error: 'Invalid email or password' });
+    }
+    const token = authMethods.generateJWT({ id: existingUser.id });
+
+    const userData = {
+      id: existingUser.id,
+      name: existingUser.name,
+      email: existingUser.email,
+    };
+    response.status(200).json({ token, user: userData });
+  } catch (error) {
+    console.log('error occurred in Login user', error);
     throw error;
   }
 };
