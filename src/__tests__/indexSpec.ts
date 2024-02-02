46cf0aa2033604199e509284eacd55432d9eefe7
Moustafa25MM <107191669+Moustafa25MM@users.noreply.github.com>
2024-02-02 17:51:07 +0200

    add unit testing for book routes

--- src/__tests__/indexSpec.ts
+++ src/__tests__/indexSpec.ts
@@ -5,12 +5,12 @@ import bcrypt from 'bcrypt';
 
 const authenticateAndGetToken = async () => {
   const credentials = {
-    email: 'testUser@example.com',
-    password: 'password',
+    email: 'admin@example.com',
+    password: 'YourSecureAdminPassword',
   };
 
   const response = await request(app).post('/auth/login').send(credentials);
-  return response.body.token;
+  return response.body.data.token;
 };
 
 describe('User Routes', () => {
@@ -79,5 +79,116 @@ describe('User Routes', () => {
       .expect(401);
 
     expect(response.body.error).toBe('Invalid email or password');
+  });
+});
+
+let token = '';
+describe('Books Controller', () => {
+  beforeAll(async () => {
+    // Authenticate and store the token before running the tests
+    token = await authenticateAndGetToken();
+
+    await prisma.book.deleteMany({});
+  });
+
+  afterAll(async () => {
+    await prisma.$disconnect();
+  });
+
+  it('should add a new book successfully', async () => {
+    const newBook = {
+      title: 'New Test Book',
+      author: 'Author Test',
+      isbn: '123-4567890123',
+      quantity: 5,
+      shelfLocation: 'A1',
+    };
+
+    const response = await request(app)
+      .post('/book/add')
+      .set('Authorization', `${token}`) // Include token in the Authorization header
+      .send(newBook)
+      .expect(201);
+
+    expect(response.body.message).toBe('Book added successfully');
+    expect(response.body.data.book).toBeDefined();
+    expect(response.body.data.book.isbn).toBe(newBook.isbn);
+  });
+
+  it('should delete a book successfully', async () => {
+    // Create a book to delete later
+    const bookToDelete = await prisma.book.create({
+      data: {
+        title: 'Book to Delete',
+        author: 'Author Test',
+        isbn: '123-delete',
+        quantity: 1,
+        shelfLocation: 'D1',
+      },
+    });
+
+    const response = await request(app)
+      .delete(`/book/delete/${bookToDelete.id}`)
+      .set('Authorization', `${token}`) // Include token in the Authorization header
+      .expect(200);
+
+    expect(response.body.message).toBe('Book deleted successfully');
+  });
+
+  it('should update a book successfully', async () => {
+    // Create a book to update later
+    const bookToUpdate = await prisma.book.create({
+      data: {
+        title: 'Original Title',
+        author: 'Original Author',
+        isbn: '123-update',
+        quantity: 10,
+        shelfLocation: 'U1',
+      },
+    });
+
+    // New data for the book
+    const updatedData = {
+      title: 'Updated Title',
+      author: 'Updated Author',
+      isbn: '123-updated',
+      quantity: 15,
+      shelfLocation: 'U2',
+    };
+
+    const response = await request(app)
+      .put(`/book/update/${bookToUpdate.id}`)
+      .set('Authorization', `${token}`)
+      .send(updatedData)
+      .expect(200);
+
+    expect(response.body.message).toBe('Book updated successfully');
+    expect(response.body.data.updatedBook.title).toBe(updatedData.title);
+  });
+
+  it('should list all books', async () => {
+    const response = await request(app)
+      .get('/book/list/books')
+      .set('Authorization', `${token}`)
+      .expect(200);
+
+    expect(response.body.message).toBe('Books retrieved successfully');
+    expect(Array.isArray(response.body.data.books)).toBeTruthy();
+  });
+
+  it('should search books by title', async () => {
+    // Assuming there are books in the database
+    const searchTerm = 'Test Book';
+    const response = await request(app)
+      .get(`/book/search/books?title=${searchTerm}`)
+      .set('Authorization', `${token}`)
+      .expect(200);
+
+    expect(response.body.message).toMatch(
+      /Books found successfully|No books found with the given criteria./
+    );
+    response.body.data.books.forEach((book: any) => {
+      expect(book.title).toContain(searchTerm);
+    });
   });
 });
